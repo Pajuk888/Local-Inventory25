65e9054c4a85b51eb1b23f13205ed8ce8e664de7
Oleg Pajuk <54703935+Pajuk888@users.noreply.github.com>
2024-12-29 12:59:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -105,5 +105,14 @@ document.addEventListener("DOMContentLoaded", () => {
     renderCombinedTotals();
   });
 
+  // Collapsible functionality
+  Array.from(document.getElementsByClassName("collapsible")).forEach(button => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+  });
+
   loadLocations();
 });
